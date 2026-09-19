25124259d26be18cbe9a41b52a98b4ab342be492
kampersanda <shnsk.knd@gmail.com>
2026-09-19 09:12:45 +0900

    Test mixed kanji and Arabic numerals

--- test/index.js
+++ test/index.js
@@ -10,3 +10,4 @@ assert.deepEqual(segmenter.segment("12３４"), ["12３４"]);
 assert.deepEqual(segmenter.segment("千二百八十"), ["千二百八十"]);
 assert.deepEqual(segmenter.segment("価格は千二百八十円です"), ["価格", "は", "千二百八十", "円", "です"]);
 assert.deepEqual(segmenter.segment("一億二千万"), ["一億二千万"]);
+assert.deepEqual(segmenter.segment("一億2000万"), ["一億", "2000", "万"]);
